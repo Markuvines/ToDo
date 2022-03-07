cbd4da3ec563029742c0a0ae6c5adb9932ccf059
Markuvines <shinigami_master@mail.ru>
2022-03-07 15:52:53 +0300

    first working prototipe, added edit function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react';
 import './App.css';
 import ToDoItem from './components/ToDoItem';
@@ -7,21 +8,21 @@ import './toDo.css'
 
 function App() {
   const [toDo, setToDo] = useState([])
+  const [task, setTask] = useState('')
+  const clickHandler = (e) => {
+    e.preventDefault();
+    setToDo([...toDo, {discription: task}]);
+    setTask('');
+  }
   return (
-    <toDoContext.Provider value={toDo, setToDo}>
+    <toDoContext.Provider value={{toDo, setToDo}}>
       <div className='container'>
       <div className='toDo'>
         <form action="post" className='form'>
-          <input type="text" className='title'/>
-          <input type="text" className='discription'/>
+          <input value={task} type="text" className='discription' onChange={(e) => setTask(e.target.value)}/>
+          <button className='postButton' onClick={e => clickHandler(e) }>Добавить задание</button>
         </form>
-
-        <ToDoItem/>
-        <ToDoItem/>
-        <ToDoItem/>
-        <ToDoItem/>
-        <ToDoItem/>
-        <ToDoItem/>
+      {toDo.map((item, index) => <ToDoItem task={item} number={index} key={index} />)}
       </div>
       </div>
     </toDoContext.Provider>
--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import toDoContext from '../contexts/toDoContext';
+//import delete from '../image/delete.svg';
+import deleteImg from '../image/delete.svg';
+import edit from '../image/edit.svg';
 
 const ToDoItem = props => {
+  const {toDo, setToDo} = useContext(toDoContext);
+  const [isEdit, setIsEdit] = useState(false);
+  const [edited , setEdited] = useState(props.task.discription);
   return (
     <div className='toDoItem'>
+      {isEdit ?<> <input type='text' value={edited} onChange={e => setEdited(e.target.value)}/> 
+      <button onClick={() => {let arr = [...toDo]
+      arr[props.number].discription = edited
+       setToDo([...arr])
+       setIsEdit(!isEdit)}}>Ок</button></>
+      :<>
       <label className='check'>
         <input className='checkbox' type="checkbox"/> 
         <span className='check__box'></span>
-        <span className='checkboxDisc'>Сходить посрать</span>
+        <span className='checkboxDisc'>{`${props.number + 1}.${props.task.discription}`}</span>
       </label>
+      <img className='checkEdit' src={edit} alt="edit" onClick={() => {
+        setIsEdit(!isEdit)
+      }}/>
+      <img className='checkDel' src={deleteImg} alt="delete" onClick={(e) => {
+        setToDo([...toDo.filter((item) => item !== props.task)]) 
+      }}/>
+      </>}
     </div>
   );
 };
